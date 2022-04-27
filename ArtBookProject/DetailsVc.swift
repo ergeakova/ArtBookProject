c963baf404b50e549a4a3589a86cc91a02c2a5ab
ergeakova <ergeak@gmail.com>
2022-04-27 17:29:45 +0300

    view design

---
+++ ArtBookProject/DetailsVc.swift
@@ -0,0 +1,25 @@
+//
+//  DetailsVc.swift
+//  ArtBookProject
+//
+//  Created by Erge Gevher Akova on 27.04.2022.
+//
+
+import UIKit
+
+class DetailsVc: UIViewController {
+
+    @IBOutlet weak var imageView: UIImageView!
+    @IBOutlet weak var lblName: UITextField!
+    @IBOutlet weak var lblArtist: UITextField!
+    @IBOutlet weak var lblYear: UITextField!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+      
+    }
+    
+    @IBAction func imageSave(_ sender: Any) {
+        
+    }
+}
--- ArtBookProject/ViewController.swift
+++ ArtBookProject/ViewController.swift
@@ -9,11 +9,10 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var tableView: UITableView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        navigationItem.title = "Art Book"
     }
-
-
 }
-
